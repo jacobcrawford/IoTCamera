b3b88ef79326c55e0e2c902c976565abc9b8098a
Jacob <jacobcrawford1@hotmail.com>
2018-11-14 19:28:01 +0100

    Setup openCV. A lot of setup needed to run the code. Follow https://www.pyimagesearch.com/2017/09/04/raspbian-stretch-install-opencv-3-python-on-your-raspberry-pi/

--- camera.py
+++ camera.py
@@ -4,6 +4,12 @@ from time import sleep
 import os, os.path
 import socket
 
+import numpy as np
+import cv2 as cv
+
+# initialize classifiers
+face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv.CascadeClassifier('haarcascade_eye.xml')
 
 # Count the number of files in folder at the start of script
 count = len([name for name in os.listdir(".")])
@@ -13,6 +19,7 @@ camera = PiCamera()
 camera.start_preview()
 
 
+# Set server ip and port for the server you want to connect to
 host = '192.168.87.105'
 port = 60000
 
@@ -21,13 +28,31 @@ while True:
 	# Make socket
 	s = socket.socket()
 	s.connect((host,port))
+
 	file_name = "./image" + str(i) + ".jpg"
-	sleep(10)
+	sleep(5)
+	# Take picture
 	camera.capture(file_name)
 	print("Captured: " + file_name)
 
-	f = open(file_name, 'rb')
+	# Analyse with openCV
+	img = cv.imread(file_name)
+	print(img)
+
+	gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+	faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+	for (x,y,w,h) in faces:
+    		cv.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+    		roi_gray = gray[y:y+h, x:x+w]
+    		roi_color = img[y:y+h, x:x+w]
+    		eyes = eye_cascade.detectMultiScale(roi_gray)
+    		for (ex,ey,ew,eh) in eyes:
+        		cv.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+	
+	cv.imwrite(file_name,img)
 	# Send file
+	f = open(file_name, 'rb')
 	l = f.read(1024)
 	while(l):
 		s.send(l)
