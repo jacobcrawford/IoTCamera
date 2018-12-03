046a62b5b53eb75c853c1e9130022cc3bbab1ec6
Jacob Crawford <jacobcrawford@hotmail.com>
2018-12-03 20:20:04 +0100

    removed redundant code

--- mqtt_camera.py
+++ mqtt_camera.py
@@ -11,15 +11,11 @@ import cv2 as cv
 face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv.CascadeClassifier('haarcascade_eye.xml')
 
-# Count the number of files in folder at the start of script
-count = len([name for name in os.listdir(".")])
-
 # Connect to the camera
 camera = PiCamera()
 camera.start_preview()
 
 #Define functions for mqtt client
-
 def on_connect(client,userdata,flags,rc):
 	print("Broker connection established")
 
@@ -31,11 +27,13 @@ client.on_connect = on_connect
 client.username_pw_set("pibroker",password="raspberry")
 client.connect(host,port,3600)
 
-i = count
+#Let camera warm up
+sleep(2)
+
 while True:
 	client.loop()
-	file_name = "./image" + str(i) + ".jpg"
-	sleep(5)
+	file_name = "./image.jpg"
+	sleep(0.1)
 	# Take picture
 	camera.capture(file_name)
 	print("Captured: " + file_name)
@@ -46,19 +44,7 @@ while True:
 	gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 	faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-	for (x,y,w,h) in faces:
-    		cv.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-    		roi_gray = gray[y:y+h, x:x+w]
-    		roi_color = img[y:y+h, x:x+w]
-    		eyes = eye_cascade.detectMultiScale(roi_gray)
-    		for (ex,ey,ew,eh) in eyes:
-        		cv.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-	
-	
-	cv.imwrite(file_name,img)
-
 	#Check if any faces are there
-	print(faces)
 	if isinstance(faces, tuple):
 		client.publish("pi/cam/value",0)
 		print("Noone there published")
@@ -66,10 +52,7 @@ while True:
 		client.publish("pi/cam/value",1)
 		print("Someone was there published")
 
-	i+=1
-	print("File sent")
-	# Close socket
 camera.stop_preview()
 
-print( "Done sending image")
+
 
